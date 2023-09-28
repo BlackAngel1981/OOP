90bc9ae21a7047e976619ba5fcd152fca360a5f8
BlackAngel1981 <130056558+BlackAngel1981@users.noreply.github.com>
2023-09-28 13:43:43 +0800

    Update chocolate.java

--- dz1/chocolate.java
+++ dz1/chocolate.java
@@ -1,1 +1,23 @@
+public class Chocolate extends Product{
+    private int calories;
 
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+
+    public Chocolate(String brand, String name, double price, int calories) {
+        super(brand, name, price);
+        this.calories = calories;
+
+    }
+
+    @Override
+    public String displayInfo() {
+        return String.format("[Шоколад] %s - %s - %.2f - Энергетическая ценность: %d", brand, name, price, calories);
+    }
+}
