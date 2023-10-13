265eba8ebc634b2ce1771096fdb45440b21b9876
BlackAngel1981 <130056558+BlackAngel1981@users.noreply.github.com>
2023-10-13 13:13:17 +0800

    Create Model.java

---
+++ dz5/presenters/Model.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+import java.util.Date;
+
+public interface Model {
+    Collection<Table> loadTables();
+    int reservationTable(Date reservationDate, int tableNo, String name);
+    int changeReservationTable(int oldReserv, int oldTable, Date reservationDate, int tableNo, String name);
+
+}
