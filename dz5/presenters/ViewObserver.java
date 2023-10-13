fad842d6b5a125c9f3177106ec3043f758bd1dda
BlackAngel1981 <130056558+BlackAngel1981@users.noreply.github.com>
2023-10-13 13:15:16 +0800

    Create ViewObserver.java

---
+++ dz5/presenters/ViewObserver.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public interface ViewObserver {
+    void onReservationTable(Date orderDate, int tableNo, String name);
+
+    void chReservationTable(int oldReserv, int oldTable, Date orderDate, int tableNo, String name);
+}
