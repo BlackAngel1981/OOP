2f5196c071db83bcc9e89b8600b14070901e151c
BlackAngel1981 <130056558+BlackAngel1981@users.noreply.github.com>
2023-10-17 13:13:28 +0800

    Update Program.java

--- dz5/Program.java
+++ dz5/Program.java
@@ -1,1 +1,27 @@
+import java.util.Date;
 
+public class Program {
+
+    /**
+     * TODO: ДОМАШНЯЯ РАБОТА
+     * Метод changeReservationTable должен ЗАРАБОТАТЬ!
+     *
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) {
+
+        int tableNum = 2, newTableNum = 3;
+
+        TableModel model = new TableModel();
+        BookingView view = new BookingView();
+        BookingPresenter bookingPresenter = new BookingPresenter(model, view);
+        bookingPresenter.updateTablesUI();
+
+        view.reservationTable(new Date(), tableNum, "Максим");
+        bookingPresenter.updateTablesUI();
+
+        view.changeReservationTable(101, tableNum, new Date(), newTableNum, "Максим");
+        bookingPresenter.updateTablesUI();
+    }
+
+}
